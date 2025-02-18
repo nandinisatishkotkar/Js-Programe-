266a32d2b4c3056c58fc2551cba3361034153262
nandinisatishkotkar <nandini@beetsol.com>
2025-02-18 05:39:23 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use.strict";//treat all Js Code as newer Version
+//alert(3+3)// We are using Node.js Not browser
+
+console.log(3+3)
+console.log("nandini");
+
+
+let name="nandini"
+let age=22
+let isLoggedIn=false
+let state;
+
+//number=>2 to power of 53
+//bigint
+//string=>""
+//boolean=>false/true
+//null=>stanslone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
+
+
---
+++ 01_basics/varibale.js
@@ -0,0 +1,12 @@
+const account_Id=1344
+let acccount_Email="nadini@gamai.com"
+var account_Password="1234"
+account_City="nashk"
+let acccount_State;
+
+//account_Id=2
+console.log(account_Id);
+acccount_Email="nan@gamail.com"
+account_City="rajshan"
+account_Password="555"
+console.table([account_Password,account_City,acccount_Email,acccount_State])
