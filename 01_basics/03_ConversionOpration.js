66dbf8535a1822443becff88c8d231591a481657
nandinisatishkotkar <nandini@beetsol.com>
2025-02-18 06:00:35 +0000

    ConversionOpration

---
+++ 01_basics/03_ConversionOpration.js
@@ -0,0 +1,23 @@
+//let score="33abcd"
+//let score=null
+let score=false
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"33"=>33
+//"33abcd"=>nan
+//true=>1;false=>0
+let isLoggedIn=""
+let booleanisLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1=>true; 0=>false;""=>false;"nandni"=>true
+ let sumNumber=33 
+let string=String(sumNumber)
+console.log(string);
+console.log(typeof string);
